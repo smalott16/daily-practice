edb706c58558adb5830828bc91c3f0ec2d306d25
Spencer Malott <spencermalott@Spencers-MBP.hitronhub.home>
2021-12-11 15:22:42 -0800

    pseudo code

---
+++ codewars/5ku-find-smallest.js
@@ -0,0 +1,11 @@
+function smallest(n) {
+  // Your code
+
+  //find index of the smallest number or created an ordered array of numbers
+
+  //if smallest is in first position move to next
+
+  //if next smallest is in next position move to next
+
+  //if smallest number is out of position, do the switch
+}
