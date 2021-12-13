24390dfe6afc72ca8184b4c07c94476e15e9a834
Spencer Malott <spencermalott16@gmail.com>
2021-12-13 00:22:52 +0000

    complete largest square in circle 7ku

---
+++ codewars/7ku-lrg-sqr-in-circle.js
@@ -0,0 +1,9 @@
+function areaLargestSquare(r) {
+  //radius is the length of the 45deg line from the centre to edge of the circle
+  //legnth are the size of the triangle made by the radius line
+  // r = sqrt(l^2 + l^2) => r = sqrt(2 * l^2)
+  // A = 2* l^2
+  // r = sqrt(A)
+  //A = r^2
+  return r * r;
+}
