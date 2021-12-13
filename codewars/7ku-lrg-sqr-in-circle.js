93bd58054786d3e3722bc1f4263a9943b96f1fce
Spencer Malott <spencermalott16@gmail.com>
2021-12-13 00:55:13 +0000

    add complete solution with comments

--- codewars/7ku-lrg-sqr-in-circle.js
+++ codewars/7ku-lrg-sqr-in-circle.js
@@ -2,8 +2,8 @@ function areaLargestSquare(r) {
   //radius is the length of the 45deg line from the centre to edge of the circle
   //legnth are the size of the triangle made by the radius line
   // r = sqrt(l^2 + l^2) => r = sqrt(2 * l^2)
-  // A = 2* l^2
-  // r = sqrt(A)
+  // A = (2* l)^2 => A = 4 * l^2
+  // r = sqrt(A/2)
   //A = r^2
-  return r * r;
+  return 2 * r * r;
 }
