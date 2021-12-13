9a8bd4d80096a44bd3fc65d086e99f545e213e60
Spencer Malott <spencermalott16@gmail.com>
2021-12-13 23:32:12 +0000

    refactor 6ku-tribonacci

--- codewars/6ku-tribonacci.js
+++ codewars/6ku-tribonacci.js
@@ -1,17 +1,14 @@
 function tribonacci(signature,n){
   //your code here
-  let count = 1;
-  let sequence = [...signature];
-
-  //edge case where sequence length is less than 3
+  
+  //edge case where signature length is less than 3
   if (n < 3) {
-    return sequence.slice(0, n);
+    return signature.slice(0, n);
   }
 
-  while (count <= n - 3) {
+  while (signature.length < n) {
     //push a sliced array that has been summed to a single value with reduce
-    sequence.push(sequence.slice(-3).reduce((prev, cur) => prev + cur));
-    count++;
+    signature.push(signature.slice(-3).reduce((prev, cur) => prev + cur));
   }
-  return sequence;
+  return signature;
 }
